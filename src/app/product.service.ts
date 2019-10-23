41b2c6262656e19c94f5b273160e5239dca252f2
eniowo <eniowo@gmail.com>
2019-10-23 20:20:36 +0100

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+//import { Map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+  private _albumUrl = '../assets/album.json';
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl)
+    .map(response => response.json());
+    //.map(response => response.json());
+  }
+
+}
