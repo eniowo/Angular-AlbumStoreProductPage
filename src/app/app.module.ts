64b3d45b309fee3d5c313e279661f18ecf441355
eniowo <eniowo@gmail.com>
2019-10-24 11:00:28 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-appRoutes: Routes[
+const appRoutes: Routes = [
   {
     path: 'products',
     component: ProductListComponent
